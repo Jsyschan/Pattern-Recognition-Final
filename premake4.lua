e99b7e0daabb235da64efe36599c07f9de88cbbc
Josh Willis <willis2342@gmail.com>
2014-11-15 11:27:27 -0500

    Premake config file

---
+++ premake4.lua
@@ -0,0 +1,41 @@
+
+solution "Final"
+  configurations { "Debug", "Release" }
+
+  configuration { "Debug" }
+    defines { "_DEBUG", "DEBUG" }
+    flags   { "Symbols", "ExtraWarnings" }
+
+  configuration { "Release" }
+    defines { "NDEBUG" }
+    flags   { "Optimize" }
+
+  platforms { "Native", "x32", "x64", "Universal" }
+
+  project "test"
+
+    kind "ConsoleApp"
+    language "C++"
+    location "example"
+    links { "pattern" }
+
+    includedirs { "include" }
+    files { "include/*.h", "example/*.cpp" }
+
+    configuration { "Debug or Release" }
+      targetdir "example/bin"
+      objdir "example/obj"
+
+  project "pattern"
+
+    kind "StaticLib"
+    language "C++"
+    location "src"
+
+    includedirs { "include" }
+    files { "include/*.h", "src/*.cpp" }
+
+    configuration { "Debug or Release" }
+      targetdir "lib"
+      objdir "src/obj"
+
